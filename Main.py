f78cb271ab7180ad2f5e93743be5318e959fca14
lividhatter <lividhatter@gmail.com>
2018-02-16 20:50:51 -0800

    Working Flask Server

--- Main.py
+++ Main.py
@@ -1,1 +1,22 @@
-x=1
+from flask import Flask, jsonify, request
+
+import requests
+import time
+
+app = Flask(__name__)
+
+messages = [{'from' : 'Cameron', 'message': 'this is the message'}]
+
+
+@app.route('/sparkbot/messages')
+def get_messages():
+    return jsonify(messages)
+
+
+@app.route('/sparkbot/messages', methods=['POST'])
+def send_message():
+    messages.append(request.get_json())
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
