eeec60165e66322fbce223affe46b63c4ced2361
lividhatter <lividhatter@gmail.com>
2018-10-28 10:32:11 -0700

    .

--- message_parse.py
+++ message_parse.py
@@ -2,36 +2,16 @@ from dateutil.parser import *
 
 
 
-am_pm_values =['am', 'Am', 'AM','pm','PM','Pm']
-hour_minute_day_values = ['D','d','M','m','h','H','day','days','Day','Days','min','Min','mins','Mins','hour','Hour','hours','Hours']
-messages = ['10/20/2018 date only reminder', '10:00am time only no space', '12:00 pm time only with space','10/11/2018 10:00am date and time','10h no time or date 10 hours','15m no time or date just minutes','3d no time or date just days']
+
+messages = ['10/20/2018 date only reminder',
+            '10:00am time only no space',
+            '12:00 pm time only with space',
+            '10/11/2018 10:00am date and time',
+            '10h no time or date 10 hours',
+            '15m no time or date just minutes',
+            '3d no time or date just days']
 
 
 
-for _ in messages:
-    found_am_pm = False
-    found_hour_min_day = False
-    print('-'*5)
-    print(_)
-    message_split = _.split(' ')
-    first_part_of_message = message_split[0]
-    
-    for item in am_pm_values:
-        if item in first_part_of_message:
-            found_am_pm = True
-            
-    if found_am_pm == False:
-        for item in hour_minute_day_values:
-            if item in first_part_of_message:
-                found_hour_min_day = True
-                print(item, first_part_of_message)
 
-        
-    if found_hour_min_day == False:
-        try:
-            Found = parse(_, fuzzy=True)
-        except:
-            print('No date or time found')
-
-        print(Found)
 
---
+++ send_test_message.py
@@ -0,0 +1,10 @@
+# This code needs to send a test message to local host that mimics the one sent by webex teams
+
+
+{'id': 'Y2lzY29zcGFyazovL3VzL01FU1NBR0UvNTJhOTE3YjAtZGFkNi0xMWU4LTk5MDUtZjE3Y2Q3ZDI5ZGJj',
+ 'roomId': 'Y2lzY29zcGFyazovL3VzL1JPT00vNmMzMmE1MjAtZWMxYi0xMWU3LWIyMDEtNGZkZjM2MzM0N2Ix',
+ 'roomType': 'group',
+ 'personId': 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS8xMjE3NGY4Yy02NDcwLTRlN2YtOWEwNy0yZGRiZWRmZTBiNTY',
+ 'personEmail': 'cameron.hughes@farmersinsurance.com',
+ 'mentionedPeople': ['Y2lzY29zcGFyazovL3VzL1BFT1BMRS9iODRhNjVkMS1hYWQ5LTQyMjAtOTgxZi1mMGMzZjBhM2UyMDk'], 
+ 'created': '2018-10-28T17:24:27.947Z'}
